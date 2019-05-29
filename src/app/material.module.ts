925e5a99ad01e5f549a96b9b57c0f756207adcd6
Ryan Mackey-Paulsen <smarth55@gmail.com>
2019-05-29 12:17:25 -0400

    update angular material deps

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatIconModule,
-  MatSidenavModule,
-  MatListModule,
-  MatRippleModule,
-  MatCardModule,
-  MatProgressBarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
